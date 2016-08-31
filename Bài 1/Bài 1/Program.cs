13640f3f11deef987a1069181afa4748fac38639
nguyenloc96 <nguyenloc96fit@gmail.com>
2016-08-31 22:13:11 +0700

    Them bai tap cs 1,2

---
+++ Bài 1/Bài 1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bài_1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            string sa, sb, sc;
+            Console.Write(" Nhap gia tri a = ");
+            sa = Console.ReadLine();
+            Console.Write(" Nhap gia tri b = ");
+            sb = Console.ReadLine();
+            Console.Write(" Nhap gia tri c = ");
+            sc = Console.ReadLine();
+            a = double.Parse(sa);
+            b = double.Parse(sb);
+            c = double.Parse(sc);
+            if ((a + b > c) && (a + c > b) && (b + c > a))
+            {
+                Console.WriteLine(" Day la 3 canh cua tam giac!");
+                if ((c * c == a * a + b * b) || (a * a == b * b + c * c) || (b * b == a * a + c * c))
+                {
+                    if ((a == b) || (b == c) || (c == a))
+                    {
+                        Console.WriteLine(" Day la tam giac vuong can!");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" Day la tam giac vuong thuong!");
+                    }
+
+                }
+                else
+                {
+                    if ((a == b) && (b == c))
+                    {
+                        Console.WriteLine(" Day la tam giac deu!");
+                    }
+                    else
+                    {
+                        if ((a == b) || (b == c) || (c == a))
+                        {
+                            Console.WriteLine(" Day la tam giac can!");
+                        }
+                        else
+                        {
+                            Console.WriteLine(" Day la tam giac thuong!");
+                        }
+                    }
+                }
+            }
+            else Console.WriteLine(" Day khong phai la 3 canh cua tam giac!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Bài 2/Bài 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bài_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double S = 0.0;
+            int n;
+            for(n = 1;(1.0/(n*n))>= 0.000001; n++)
+            {
+                S += (1.0 / (n*n));
+            }
+            Console.WriteLine(" S = {0}", S);
+            Console.WriteLine(" N = {0}", n);
+            Console.WriteLine(" PI = {0}", Math.Sqrt(6*S));
+            Console.ReadKey();
+        }
+    }
+}
